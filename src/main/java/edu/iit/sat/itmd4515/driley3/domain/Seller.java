165989c103aae4c0e8b7909376e4194f49accd30
RileyDaria <driley3@hawk.iit.edu>
2017-03-27 17:19:48 -0500

    Fixed named query

--- src/main/java/edu/iit/sat/itmd4515/driley3/domain/Seller.java
+++ src/main/java/edu/iit/sat/itmd4515/driley3/domain/Seller.java
@@ -12,6 +12,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -21,31 +23,54 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "seller")
+@NamedQueries(
+        {
+            @NamedQuery(name = "Seller.findAll", query = "select s from Seller s")})
 public class Seller extends NamedEntity implements Serializable {
 
     @OneToOne
     @JoinColumn(name = "inventory_id")
     private Inventory inventory;
 
+    /**
+     *
+     * @param firstName
+     * @param lastName
+     */
     public Seller(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    /**
+     *
+     * @return
+     */
     public Inventory getInventory() {
         return inventory;
     }
 
+    /**
+     *
+     * @param inventory
+     */
     public void setInventory(Inventory inventory) {
         this.inventory = inventory;
 //        inventory.setSeller(this);
     }
 
+    /**
+     *
+     * @param i
+     */
     public void addInventory(Inventory i) {
         this.inventory = i;
         i.setSeller(this);
     }
 
+    /**
+     *
+     */
     public Seller() {
     }
 
